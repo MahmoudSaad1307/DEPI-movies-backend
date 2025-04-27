e369d7439c942e382e35bf9f4bbdfff56ad40757
Malik1307 <mahmoud2004saad@gmail.com>
2025-04-27 20:21:03 +0300

    koyab deployment

--- .history/app_20250427202043.js
+++ .history/app_20250427202043.js
@@ -31,5 +31,5 @@ app.use("/api/userList", userListRoute);
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on Port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
